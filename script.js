013bf164c1f91cf10e08b2bb03dc458f276b54e1
shimon <shimonox74@gmail.com>
2019-10-24 23:13:39 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,27 @@
+//'use strict';
+
+a = 5;
+console.log(a);
+
+//alert("Hollo World!!!");
+
+/*let answer = +prompt("Есть ли вам 18?");
+
+ console.log(typeof(answer));
+*/
+//let incr = 10,
+   // decr = 10;
+
+
+
+/*console.log(incr++);
+console.log(decr--);
+ console.log(5%2);
+ console.log("2" === 2);
+
+ let isChecked = true,
+     isClose = false;
+
+     console.log(isChecked || isClose);
+*/
+
